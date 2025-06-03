10d232a30dcb617abffa6f802a5240e7b5ab3dbf
N.NAVATEJA REDDY <navatejar80@gmail.com>
2025-06-03 14:11:47 +0530

    Delete src/crm/crm directory

--- src/crm/crm/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login form', () => {
-  render(<App />);
-  const loginElements = screen.getAllByText(/login/i);
-  expect(loginElements.length).toBeGreaterThan(0);
-});
